5e7df2babbb95e0e97aba83d8d90831c4771e526
lr <3399075402@qq.com>
2024-03-28 23:09:38 +0800

    practice

---
+++ HashTable/CanConstruct.java
@@ -0,0 +1,29 @@
+package HashTable;
+
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+
+        int[] record = new int[26];//
+
+        for(char c : magazine.toCharArray()){//toCharArray：将字符串转化为字符数组
+            record[c - 'a'] += 1;
+        }
+
+        for(char c : ransomNote.toCharArray()){
+            record[c - 'a'] -= 1;
+        }
+
+        // 如果数组中存在负数，说明ransomNote字符串总存在magazine中没有的字符
+        for(int i : record){
+            if(i < 0){//在增强型 for 循环中，i 是数组中的元素值，而不是索引,所以不是record[i]<0
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ HashTable/FourSumCount.java
@@ -0,0 +1,26 @@
+package HashTable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        int res = 0;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        //统计两个数组中的元素之和，同时统计出现的次数，放入map
+        for (int i : nums1) {
+            for (int j : nums2) {
+                int sum = i + j;
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+        //统计剩余的两个元素的和，在map中找是否存在相加为0的情况，同时记录次数
+        for (int i : nums3) {
+            for (int j : nums4) {
+                int sum2=0-i-j;
+                res += map.getOrDefault(sum2, 0);
+            }
+        }
+        return res;
+    }
+}
