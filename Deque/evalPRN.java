87c765c0c5227538c635c1dca9802a3eb57d72e2
3399075402@qq.com <3399075402@qq.com>
2024-10-13 23:23:42 +0800

    practice

--- Deque/evalPRN.java
+++ Deque/evalPRN.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int[] result=new int[temperatures.length];
@@ -16,5 +18,21 @@ class Solution {
             }
         }
         return result;
+    }
+}
+
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp=new int[amount+1];
+        dp[0]=1;
+
+        for(int i=0;i<coins.length;i++){
+            for(int j=coins[i];j<=amount;j++){
+                dp[j]=dp[j]+dp[j-coins[i]];
+            }
+        }
+
+        return dp[amount];
     }
 }
